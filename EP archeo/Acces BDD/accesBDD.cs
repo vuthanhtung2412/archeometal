d29a709a255fddba0ec51fd717c3481100603343
mhillair <matthieu.hillairet@insa-rennes.fr>
2022-04-06 18:21:03 +0200

    Signatures des fonctions d'acces aux objets en lecture related #18

---
+++ EP archeo/Acces BDD/accesBDD.cs
@@ -0,0 +1,24 @@
+
+
+public class AccesBDD {
+    
+    //Récupération d'un objet par son identifiant 
+    objectArcheo getByID(int id){
+    }
+
+    //Récupération des objets lié à un tag
+    objectArcheo[] getByTag(Tag tag){
+
+    }
+
+    //Récupération de tous les objets visibles une vue
+    objectArcheo[] getByView(int view){
+
+    }
+
+    //Récupération d'un objet grâce à son nom
+    objectArcheo getByNom(string nom){
+
+    }
+
+}
