b286f1bb0687cce2f9f50e34f5a372fe7116d023
Thibaut <thibaut.raoul@icloud.com>
2022-04-19 15:46:20 +0200

    Implémentation des fonctions de la classe Tag

--- EP archeo/Assets/Scripts/Acces BDD/Tag.cs
+++ EP archeo/Assets/Scripts/Acces BDD/Tag.cs
@@ -1,8 +1,3 @@
-using UnityEngine;
-using System.Data;
-using Mono.Data.Sqlite;
-using System.IO;
-
 namespace bdd_ep
 {
 
@@ -11,9 +6,8 @@ namespace bdd_ep
     /// </summary>
     public class Tag
     {
-
-        string tagName;
-        string parentName;
+        public string tagName;
+        public string parentName;
 
         /// <summary>
         /// Création d'un Tag
@@ -32,9 +26,14 @@ namespace bdd_ep
         /// <param name="idObj">Identifiant de l'objet archéologique</param>
         /// <param name="tagName">Nom du tag</param>
         /// <returns>True si et seulement si tout s'est déroulé correctement</returns>
-        static bool associateObjectToTag(int idObj, string tagName)
+        public static bool associateObjectToTag(int idObj, string tagName)
         {
-            return false;
+            if ((Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{tagName}';").Rows.Count == 0) ||
+                (Database.DataReader($"SELECT * FROM obj WHERE id_obj = '{idObj}';").Rows.Count == 0) || (Database
+                    .DataReader($"SELECT * FROM tag_map WHERE tag_name = '{tagName}' AND id_obj='{idObj}';").Rows
+                    .Count != 0)) return false;
+            Database.DataWriter($"INSERT INTO tag_map (tag_name, id_obj) VALUES ('{tagName}','{idObj}');");
+            return true;
         }
 
         /// <summary>
@@ -43,9 +42,20 @@ namespace bdd_ep
         /// <param name="tagName">Nom du nouveau Tag</param>
         /// <param name="parentName">Nom du tag parent (peut être NULL)</param>
         /// <returns>True si et seulement si tout s'est déroulé correctement</returns>
-        static bool createNewTag(string tagName, string parentName)
+        public static bool createNewTag(string tagName, string parentName)
         {
-            return false;
+            // Si le tag existe déjà : on n'insert rien
+            if (Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{tagName}';").Rows.Count != 0) return false;
+            if (parentName == "")
+            {
+                Database.DataWriter($"INSERT INTO tag (tag_name) VALUES ('{tagName}');");
+            }
+            else
+            {
+                Database.DataWriter($"INSERT INTO tag (tag_name, parent_tag) VALUES ('{tagName}','{parentName}');");
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -54,10 +64,14 @@ namespace bdd_ep
         /// <param name="parentName">Nom du Tag parent</param>
         /// <param name="childName">Nom du Tag enfant</param>
         /// <returns>True si et seulement si tout s'est déroulé correctement</returns>
-        static bool declareTagPaternity(string parentName, string childName)
+        public static bool declareTagPaternity(string parentName, string childName)
         {
-            return false;
+            // L'ascendant ou le descendant n'existe pas dans la base de donnée : on ne fait rien
+            if (Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{childName}';").Rows.Count == 0 ||
+                Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{parentName}';").Rows.Count == 0)
+                return false;
+            Database.DataWriter($"UPDATE tag SET parent_tag = '{parentName}' WHERE tag_name = '{childName}';");
+            return true;
         }
     }
-
 }
