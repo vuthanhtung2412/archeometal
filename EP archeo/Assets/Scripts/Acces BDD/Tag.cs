fb6cce5d4832f17c454e5e7f65d4a8b2be93a29f
Thibaut <thibaut.raoul@icloud.com>
2022-05-02 18:34:34 +0200

    Modifications et commentaires en anglais sur Tag

--- EP archeo/Assets/Scripts/Acces BDD/Tag.cs
+++ EP archeo/Assets/Scripts/Acces BDD/Tag.cs
@@ -1,77 +1,232 @@
-namespace bdd_ep
+using System.Data;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic.FileIO;
+
+namespace bdd_ep;
+
+/// <summary>
+/// Manage tags describing archaeological objects
+/// </summary>
+public class Tag
 {
+    private int _idTag;
+    private string _tagName;
+    private string _parentName;
 
     /// <summary>
-    /// Gestion des tags décrivant les objets archéologiques
+    /// Create a tag
     /// </summary>
-    public class Tag
+    /// <param name="idTag">Tag id</param>
+    /// <param name="tagName">Tag's name</param>
+    /// <param name="parentName">Parent tag's name (may be empty)</param>
+    private Tag(int idTag, string tagName, string parentName)
     {
-        public string tagName;
-        public string parentName;
+        this._idTag = idTag;
+        this._tagName = tagName;
+        this._parentName = parentName;
+    }
 
-        /// <summary>
-        /// Création d'un Tag
-        /// </summary>
-        /// <param name="tagName">Nom du tag</param>
-        /// <param name="parentName">Nom du tag parent (peut être NULL)</param>
-        public Tag(string tagName, string parentName)
+    /// <summary>
+    /// Associate a archaeological object to a tag
+    /// </summary>
+    /// <param name="idObj">Archaeological object id (integer)</param>
+    /// <param name="idTag">Tag id</param>
+    /// <returns>True if and only if everything went well</returns>
+    public static bool AssociateObjectToTag(int idObj, int idTag)
+    {
+        if ((Database.DataReader($"SELECT * FROM tag WHERE id_tag = {idTag};").Rows.Count == 0) ||
+            (Database.DataReader($"SELECT * FROM obj WHERE id_obj = {idObj};").Rows.Count == 0) || (Database
+                .DataReader($"SELECT * FROM tag_map WHERE id_tag = {idTag} AND id_obj={idObj};").Rows
+                .Count != 0)) return false;
+        Database.DataWriter($"INSERT INTO tag_map (id_tag, id_obj) VALUES ({idTag},{idObj});");
+        Console.WriteLine($"INSERT INTO tag_map (id_tag, id_obj) VALUES ({idTag},{idObj});");
+        return true;
+    }
+
+    /// <summary>
+    /// Create a new tag
+    /// </summary>
+    /// <param name="tagName">New tag's name</param>
+    /// <param name="parentName">Parent tag's name (may be empty)</param>
+    /// <returns>The new tag. Return null if something went wrong.</returns>
+    public static Tag? CreateNewTag(string tagName, string parentName)
+    {
+        tagName = Database.DoubleApostropheAndRemoveSemiColumn(tagName);
+        parentName = Database.DoubleApostropheAndRemoveSemiColumn(parentName);
+        // If the tag exist : do not insert anything
+        if (Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{tagName}' AND parent_tag = '{parentName}';")
+                .Rows.Count != 0) return null;
+        Database.DataWriter($"INSERT INTO tag (tag_name, parent_tag) VALUES ('{tagName}','{parentName}');");
+
+        // Get tag id in Db
+        var idTagInDb = int.Parse(Database
+            .DataReader($"SELECT id_tag FROM tag WHERE tag_name = '{tagName}' AND parent_tag = '{parentName}';").Rows[0]
+            .ItemArray
+            .Select(x => x.ToString()).ToArray()[0]);
+
+        return new Tag(idTagInDb, tagName, parentName);
+    }
+
+    /// <summary>
+    /// Delete a tag from the database
+    /// </summary>
+    /// <param name="tagName">Tag name to delete</param>
+    public static void DeleteTag(string tagName)
+    {
+        tagName = Database.DoubleApostropheAndRemoveSemiColumn(tagName);
+        // Delete all children
+        var tagsToDelete = Database.DataReader($"SELECT * FROM tag WHERE parent_tag = '{tagName}';");
+        foreach (DataRow tag in tagsToDelete.Rows)
         {
-            this.tagName = tagName;
-            this.parentName = parentName;
+            var tagNameInString = tag["tag_name"].ToString();
+            // Delete all children
+            if (tagNameInString != null)
+                DeleteTag(tagNameInString);
         }
 
-        /// <summary>
-        /// Associer un objet archéologique à un Tag
-        /// </summary>
-        /// <param name="idObj">Identifiant de l'objet archéologique</param>
-        /// <param name="tagName">Nom du tag</param>
-        /// <returns>True si et seulement si tout s'est déroulé correctement</returns>
-        public static bool associateObjectToTag(int idObj, string tagName)
-        {
-            if ((Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{tagName}';").Rows.Count == 0) ||
-                (Database.DataReader($"SELECT * FROM obj WHERE id_obj = '{idObj}';").Rows.Count == 0) || (Database
-                    .DataReader($"SELECT * FROM tag_map WHERE tag_name = '{tagName}' AND id_obj='{idObj}';").Rows
-                    .Count != 0)) return false;
-            Database.DataWriter($"INSERT INTO tag_map (tag_name, id_obj) VALUES ('{tagName}','{idObj}');");
-            return true;
-        }
+        // Delete the tag
+        Database.DataWriter($"DELETE FROM tag WHERE tag_name = '{tagName}'");
+    }
 
-        /// <summary>
-        /// Créer un nouveau Tag
-        /// </summary>
-        /// <param name="tagName">Nom du nouveau Tag</param>
-        /// <param name="parentName">Nom du tag parent (peut être NULL)</param>
-        /// <returns>True si et seulement si tout s'est déroulé correctement</returns>
-        public static bool createNewTag(string tagName, string parentName)
-        {
-            // Si le tag existe déjà : on n'insert rien
-            if (Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{tagName}';").Rows.Count != 0) return false;
-            if (parentName == "")
-            {
-                Database.DataWriter($"INSERT INTO tag (tag_name) VALUES ('{tagName}');");
-            }
-            else
-            {
-                Database.DataWriter($"INSERT INTO tag (tag_name, parent_tag) VALUES ('{tagName}','{parentName}');");
-            }
 
-            return true;
-        }
+    /// <summary>
+    /// Declare paternity between two tags
+    /// </summary>
+    /// <param name="parentName">parent tag's name</param>
+    /// <param name="childName">child tag's name</param>
+    /// <returns>False if and only if nothing as be done : parent or child does not exist in the database</returns>
+    public static bool DeclareTagPaternity(string parentName, string childName)
+    {
+        parentName = Database.DoubleApostropheAndRemoveSemiColumn(parentName);
+        childName = Database.DoubleApostropheAndRemoveSemiColumn(childName);
+        // Of ascending or descendant does not exist : do nothing and return false
+        if (Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{childName}';").Rows.Count == 0 ||
+            Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{parentName}';").Rows.Count == 0)
+            return false;
+        Database.DataWriter($"UPDATE tag SET parent_tag = '{parentName}' WHERE tag_name = '{childName}';");
+        return true;
+    }
 
-        /// <summary>
-        /// Déclaration d'une relation de paternité entre deux Tags
-        /// </summary>
-        /// <param name="parentName">Nom du Tag parent</param>
-        /// <param name="childName">Nom du Tag enfant</param>
-        /// <returns>True si et seulement si tout s'est déroulé correctement</returns>
-        public static bool declareTagPaternity(string parentName, string childName)
+    /// <summary>
+    /// Import a set of tags from a csv file
+    /// </summary>
+    /// <param name="pathToFile">Path to the csv file</param>
+    /// <returns>True if and only if everything went well</returns>
+    public static bool ImportCsvToDataBase(string pathToFile)
+    {
+        // Regex to delete all char before the first capital letter
+        const string filterRegex = @"(.*)[A-Z]";
+
+        // Set parser options
+        using var parser = new TextFieldParser(pathToFile);
+        parser.TextFieldType = FieldType.Delimited;
+        parser.SetDelimiters(";");
+
+        // Store in an array
+        var globalList = new List<string[]>();
+
+        // Read the csv file
+        while (!parser.EndOfData)
         {
-            // L'ascendant ou le descendant n'existe pas dans la base de donnée : on ne fait rien
-            if (Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{childName}';").Rows.Count == 0 ||
-                Database.DataReader($"SELECT * FROM tag WHERE tag_name = '{parentName}';").Rows.Count == 0)
+            var rows = parser.ReadFields();
+
+            if (rows == null)
                 return false;
-            Database.DataWriter($"UPDATE tag SET parent_tag = '{parentName}' WHERE tag_name = '{childName}';");
-            return true;
+
+            // Delete the indexes
+            rows = DeleteRegexInArray(rows, filterRegex);
+
+            // Add line in the list
+            if (rows != null)
+            {
+                globalList.Add(rows);
+            }
         }
+
+        InsertListToDatabase(globalList);
+        return false;
+    }
+
+    /// <summary>
+    /// Insert a list of arrays (Catégorie fonctionnelle;Sous-catégorie fonctionnelle;Groupe fonctionnel) into the database
+    /// </summary>
+    /// <param name="stringsList">List of arrays to insert</param>
+    private static void InsertListToDatabase(List<string[]> stringsList)
+    {
+        // Variables in CSV file
+        var categorieFonctionnelle = "";
+        var sousCategorieFonctionnelle = "";
+        var groupeFonctionnel = "";
+
+        // Delete table header
+        var firstItemOfList = stringsList.First();
+        var header1 = firstItemOfList.First();
+        if (header1.Equals("Catégorie fonctionnelle"))
+            stringsList.Remove(firstItemOfList);
+
+        // Iterate on the array and insert into database
+        var globalArray = stringsList.ToArray();
+        foreach (var t in globalArray)
+        {
+            for (var j = 0; j < t.Length; j++)
+            {
+                // If we find a new categorie fonctionnelle
+                if (j == 0 && t[j] != "" && t[j] != categorieFonctionnelle)
+                {
+                    categorieFonctionnelle = t[j];
+                    CreateNewTag(categorieFonctionnelle, "");
+                }
+
+                // If we find a new sous-categorie fonctionnelle
+                if (j == 1 && t[j] != "" && t[j] != sousCategorieFonctionnelle)
+                {
+                    sousCategorieFonctionnelle = t[j];
+                    CreateNewTag(sousCategorieFonctionnelle, categorieFonctionnelle);
+                }
+
+                // If we find a new groupe fonctionnel
+                if (j == 2 && t[j] != "" && t[j] != groupeFonctionnel)
+                {
+                    groupeFonctionnel = t[j];
+                    CreateNewTag(groupeFonctionnel, sousCategorieFonctionnelle);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Delete an expression asking to the first group of a regex in each cell of a table
+    /// </summary>
+    /// <param name="rows">Array of strings</param>
+    /// <param name="filterRegex">Regex with one or more groups</param>
+    /// <returns>Array of strings where each occurence of the regex has been delete</returns>
+    private static string[]? DeleteRegexInArray(string[] rows, string filterRegex)
+    {
+        for (var i = 0; i < rows.Length; i++)
+        {
+            // Only where the string is not empty
+            if (rows[i].Equals("")) continue;
+            foreach (Match match in Regex.Matches(rows[i], filterRegex))
+            {
+                if (match.Groups[1].Value != "")
+                {
+                    // Delete the first group
+                    rows[i] = rows[i].Replace(match.Groups[1].Value, "");
+                }
+            }
+        }
+
+        return rows;
+    }
+
+    /// <summary>
+    /// Get all objects id associated to the tag
+    /// </summary>
+    /// <param name="idTag">Tag id</param>
+    /// <returns>List of all objects id associated to the tag</returns>
+    public static List<int> GetObjectsAssociatedWithTag(int idTag)
+    {
+        var queryResult = Database.DataReader($"SELECT id_obj FROM tag_map WHERE id_tag = {idTag}");
+        return (from DataRow row in queryResult.Rows select int.Parse(row["id_obj"].ToString()!)).ToList();
     }
 }
