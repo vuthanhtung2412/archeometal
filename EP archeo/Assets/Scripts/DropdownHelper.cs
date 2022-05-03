d942c0e531f2b6aa97ef47758daf30c0115b7852
Dorian Humeau <Dorian.humeau@insa-rennes.fr>
2022-05-03 18:02:33 +0200

    Add dropdown animation

--- EP archeo/Assets/Scripts/DropdownHelper.cs
+++ EP archeo/Assets/Scripts/DropdownHelper.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class DropdownHelper : MonoBehaviour
 {
-	
+	public float animationDuration;
 	public Toggle[] toggleList;
 	public GameObject[] childs;
 	public bool[] oldStatuses;
@@ -17,16 +17,22 @@ public class DropdownHelper : MonoBehaviour
 			tgl.isOn = val;
 	}
 	
+	private Animator anim;
+	void Start() {
+		anim = GetComponentInChildren<Animator>();
+	}
+	
 	public void onClick() {
 		if (isEnabled) {
 			for (int i = 1; i < childs.Length; i++) { // We start at 1 so we don't disable ourself
 				oldStatuses[i] = childs[i].activeInHierarchy;
 				childs[i].SetActive(false);
-				
+				anim.CrossFade("Base Layer.DropdownRight", animationDuration);
 			}
 		} else {
 			for (int i = 1; i < childs.Length; i++) {
 				childs[i].SetActive(oldStatuses[i]);
+				anim.CrossFade("Base Layer.DropdownDown", animationDuration);
 			}
 		}
 		isEnabled = !isEnabled;
